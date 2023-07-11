e2693cdfded00a2b02363eb93d5acc5936c85463
amirhosein tahmasbzadeh <amirt3707@gmail.com>
2023-07-11 12:04:09 +0330

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,11 +5,11 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
-  defaultNetwork: "ganache",
+  defaultNetwork: "polygon",
   networks: {
-    ganache: {
-      // chainId: 5777,
-      url: "http://127.0.0.1:7545",
+    polygon: {
+      chainId: 137,
+      url: "https://matic.getblock.io/83ac5e9c-b4ae-48e5-932b-ebf37bbfaf3b/mainnet",
       accounts: [
         "0x8a300b8da5df26967d844e2db52b2eedf5e16905577c577823b3ae9c15271580",
       ],
